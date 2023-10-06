2256dec359af825733733dbb2f937bcf31a86ccc
Martín Isusi Seff <martinisusi@hotmail.com>
2023-10-06 16:55:17 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,32 +4,37 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var players = [
-    'Player 1'
+var samplePlayers = [
+      'Player 1'
     , 'Player 2'
     , 'Player 3'
+    , 'Player 4'
+    , 'Player 5'
 ];
-base_degrees = 360 / players.length;
-current_rotation = base_degrees
-players.forEach((player) => {
-    new_label = `<div class="roulette-wheel-label" style='transform: rotate(${current_rotation - (base_degrees / 2)}deg); transform-origin: bottom center'>${player}</div>`;
-    new_separator = `<div class="roulette-wheel-separator" style='transform: rotate(${current_rotation}deg); transform-origin: bottom center'></div>`;
-    current_rotation += base_degrees
-    $('#roulette-container').append(new_label);
-    $('#roulette-container').append(new_separator);
-});
+
+function drawRoulette(players) {
+    base_degrees = 360 / players.length;
+    current_rotation = base_degrees
+    players.forEach((player) => {
+        new_label = `<div class="roulette-wheel-label" style='transform: rotate(${current_rotation - (base_degrees / 2)}deg); transform-origin: bottom center'>${player}</div>`;
+        new_separator = `<div class="roulette-wheel-separator" style='transform: rotate(${current_rotation}deg); transform-origin: bottom center'></div>`;
+        current_rotation += base_degrees
+        $('#roulette-container').append(new_label);
+        $('#roulette-container').append(new_separator);
+    });
+}
+
 
 var lastDegrees = 0;
 
 function spin() {
     var toDegrees = getRandomInt(360 * 3, 360 * 6) + lastDegrees;
-
     $('#button-spin').prop('disabled', 'true');
     $('#button-spin').css('background-color', 'gray');
     $('#button-spin').css('color', 'black');
     $('#button-spin').text('...');
     $('#button-spin').css('top', 'calc( 100% - 98px )');
-
+    
     $.keyframe.define([{
         name: 'roulette',
         from: {
@@ -59,19 +64,50 @@ function spin() {
     });
 }
 
-$('#button-spin').on('click', spin);
+function editPlayer(event){
+    var playerId = event.currentTarget.id.split('-')[0];
+    $(`#${playerId}-label`).prop('disabled', false);
+    $(`#${playerId}-label`).focus();
+    event.currentTarget.innerHTML = '<i class="fas fa-check"></i>';
+    $(event.currentTarget).off('click', editPlayer);
+    $(event.currentTarget).on('click', submitPlayerEdit);
+}
 
+function submitPlayerEdit(event){
+    var playerId = event.currentTarget.id.split('-')[0];
+    event.currentTarget.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+    $(`#${playerId}-label`).prop('disabled', true);
+    $(event.currentTarget).off('click', submitPlayerEdit);
+    $(event.currentTarget).on('click', editPlayer);
+    var players = [];
+    $('.player').map((player) => {
+        var label_id = `#${$('.player')[player].id}-label`
+        players.push($(label_id)[0].value);
+    });
+    $('#roulette-container').empty();
+    $('#roulette-container').append('<div class="roulette-wheel" id="roulette-wheel"></div>')
+    drawRoulette(players);
+}
 
+function deletePlayer(event){
+    var playerId = event.currentTarget.id.split('-')[0];
+    $(`#${playerId}`).delete();
+}
 
 $(document).ready(function () {
-    var rouletteHeight = ($('#roulette-container').position().top - 15)
+    drawRoulette(samplePlayers);
+    var rouletteHeight = ($('#roulette-container').position().top - 15);
 
     if (window.devicePixelRatio > 2) {
         rouletteHeight = ($('#roulette-container').position().top - (15)) * (window.devicePixelRatio - 1)
     }
 
-    $('#triangle').css('top', `${rouletteHeight}px`)
-    console.log(`-${rouletteHeight / 4}px`);
-    $('#roulette-container')
+    $('#triangle').css('top', `${rouletteHeight}px`);
+    $('#button-spin').on('click', spin);
 
+    new SimpleBar(document.querySelector('.player-list-scroll'));
+
+    $('.edit-button').on('click', editPlayer);
+    $('.delete-button').on('click', deletePlayer);
+    
 });
